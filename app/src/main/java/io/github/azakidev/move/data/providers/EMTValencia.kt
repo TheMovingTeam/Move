8015a5414571d94b9d9a13cad6ea5e9c4d91ef0f
Azakidev <zazaguichi@outlook.com>
2025-11-03 17:28:38 +0100

    Clean up EMTValencia parser more

--- app/src/main/java/io/github/azakidev/move/data/providers/EMTValencia.kt
+++ app/src/main/java/io/github/azakidev/move/data/providers/EMTValencia.kt
@@ -1,6 +1,5 @@
 package io.github.azakidev.move.data.providers
 
-import android.util.Xml
 import io.github.azakidev.move.data.LineTime
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -8,14 +7,6 @@ import kotlinx.serialization.decodeFromString
 import nl.adaptivity.xmlutil.serialization.XML
 import nl.adaptivity.xmlutil.serialization.XmlCData
 import nl.adaptivity.xmlutil.serialization.XmlElement
-import nl.adaptivity.xmlutil.serialization.XmlId
-import nl.adaptivity.xmlutil.serialization.XmlIgnoreWhitespace
-import nl.adaptivity.xmlutil.serialization.XmlOtherAttributes
-import nl.adaptivity.xmlutil.serialization.XmlValue
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserException
-import java.io.IOException
-import java.io.StringReader
 import kotlin.collections.component1
 import kotlin.collections.component2
 
@@ -53,7 +44,7 @@ fun parseEMTValencia(
 
     val timeList = mutableListOf<LineTime>()
 
-    estimatesByDestination.forEach { (_, value) ->
+    estimatesByDestination.forEach { (destination, value) ->
         val estimates = value.groupBy { it.line }
         timeList += estimates.mapNotNull { (key, value) ->
             val matchedLine = key
@@ -65,14 +56,14 @@ fun parseEMTValencia(
                 if (value.count() >= 2) {
                     LineTime(
                         lineId = matchedLine,
-                        destination = value[0].destination,
+                        destination = destination,
                         nextTimeFirst = value[0].min.split(" ").first().toIntOrNull() ?: 0,
                         nextTimeSecond = value[1].min.split(" ").first().toIntOrNull() ?: 0
                     )
                 } else {
                     LineTime(
                         lineId = matchedLine,
-                        destination = value[0].destination,
+                        destination = destination,
                         nextTimeFirst = value[0].min.split(" ").first().toIntOrNull() ?: 0,
                     )
                 }
