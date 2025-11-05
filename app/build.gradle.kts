74edef1f9c76600ada8a65bcdc166d68396f2dd3
Azakidev <zazaguichi@outlook.com>
2025-11-05 09:56:13 +0100

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "io.github.azakidev.move"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.11"
+        versionCode = 12
+        versionName = "0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
